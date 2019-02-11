6d1480aeae3950dc047137efbba84442a33aed10
Simon Tharby <jinjamail@gmail.com>
2019-02-11 17:11:21 +0000

    implement basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_back(fallback_location: root_path)
+      # redirect_to posts (not implemented yet)
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    #redirect_to root_url
+    redirect_back(fallback_location: root_path)
+  end
 end
