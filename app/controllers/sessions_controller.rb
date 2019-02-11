bc6cf8473c0bd14197a04afd1d9cad6499b33111
Simon Tharby <jinjamail@gmail.com>
2019-02-11 19:23:00 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       redirect_back(fallback_location: root_path)
       # redirect_to posts (not implemented yet)
     else
@@ -15,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     #redirect_to root_url
     redirect_back(fallback_location: root_path)
   end
