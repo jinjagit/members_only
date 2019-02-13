ad689c7d6a47e11e1b57eda5c7943e09ab4ed82d
Simon Tharby <jinjamail@gmail.com>
2019-02-13 15:38:01 +0000

    remove redundant repetition (of before-filter)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
   def new
-    if logged_in?
-      @post = Post.new
-    else
-      redirect_to login_path
-    end
+    @post = Post.new
   end
 
   def create
